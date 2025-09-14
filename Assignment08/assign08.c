ca13fd2578340f80c6138468b9a7f58b51fbe0ba
eunsoo <ehfl0489@gmail.com>
2025-09-15 07:01:03 +0900

    push

---
+++ Assignment08/assign08.c
@@ -0,0 +1,14 @@
+/*
+ 파일명: Assignment08-c
+ 내용: 상품 가격이 저장된 정수형 배열에 대하여 할인율(%)을 입력받아 할인된 가격을
+ 계산해서 출력하는 프로그램을 작성하시오
+ 작성자: 이은수
+ 날짜: 2025.09.15
+ 버전 : v0.2
+ */
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#define ARR_SIZE 5
+#include <stdio.h>
+
